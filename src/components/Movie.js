4adacbddb1c341b25851b5c8300aa113ecd172e6
Daniel Santander <danyvo1d@gmail.com>
2020-11-02 15:15:28 -0600

    test: add movie component tests

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,7 +5,7 @@ import { withRouter, Link } from 'react-router-dom';
 const Movie = ({ movie }) => (
   <Link to={`/movie/${movie.imdbID}`} id="link-movie" data-testid="movie_card">
     <div className="card movie">
-      <img alt="" className="card-img-top" src={movie.Poster} />
+      <img alt="" className="card-img-top" src={movie.Poster} data-testid="Poster" />
       <div className="card-body">
         <h5 className="card-title">{movie.Title}</h5>
         <p className="card-text">{movie.Year}</p>
