33d4753d514b8ff2ea8cb36d99a1a4a32aa316df
Daniel Santander <danyvo1d@gmail.com>
2020-11-02 14:49:19 -0600

    feat: add filter reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,8 @@
-/* import { combineReducers } from 'redux';
-import moviesReducer from './movies';
+import { combineReducers } from 'redux';
 import filterReducer from './filter';
 
 const rootReducer = combineReducers({
-  movies: moviesReducer,
   filter: filterReducer,
 });
 
 export default rootReducer;
- */
